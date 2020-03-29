5f5d6ddb7cd01152b2529958e58ac857408fb20a
vijayamaravajhala <59028747+vijayamaravajhala@users.noreply.github.com>
2020-03-29 17:59:56 +0400

    Add files via upload

---
+++ Sample_Capstone_Project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> <center> Coursera Capstone Project </center> </h1>
+
+# <h3> This is a sample Capstone Project Notebook created for the GitHub interaction </h3>
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
